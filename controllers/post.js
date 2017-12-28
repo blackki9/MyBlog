09f3b06e00c2b3c277a03db2f090358db97fa1ef
Vyacheslav Okulov <vokulov@intersog.com>
2017-12-28 18:46:37 +0200

    Refactoring router code

---
+++ controllers/post.js
@@ -0,0 +1,72 @@
+const Post = require("../models/post");
+
+exports.getAllPosts = (req,res) => {
+    Post.find( (err, posts) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(posts);
+    });
+};
+
+exports.getPostById = (req,res) => {
+   const id = req.params.id;
+   Post.findById(id, (err, foundPost) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(foundPost);
+   });
+};
+
+exports.createNewPost = (req,res) => {
+    const blogPost = req.body;
+
+    // TODO validation
+    const post = new Post();
+    post.title = blogPost.title;
+    post.text = blogPost.text;
+
+    post.save( (err) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json({message: "Post was added", data: post});
+    } );
+};
+
+exports.updatePost = (req,res) => {
+    const id = req.params.id;
+    Post.findById(id, (err, foundPost) => {
+        if (err) {
+            res.send(err);
+        }
+
+        const title = req.body.title;
+        const text = req.body.text;
+
+        if (title) {
+            foundPost.title = title;
+        }
+
+        if (text) {
+            foundPost.text = text;
+        }
+
+        res.json(foundPost);
+    });
+};
+
+exports.deletePost = (req,res) => {
+    const id = req.params.id;
+    Post.findByIdAndRemove(id, (err) => {
+        if (err) {
+            res.send(err);
+        }
+
+        res.json({message: "Post was deleted"});
+    })
+};
--- routes/posts.js
+++ routes/posts.js
@@ -1,78 +1,16 @@
+
 const express = require("express");
 const router = express.Router();
 const Post = require("../models/post");
+const postController = require("../controllers/post");
 
-//TODO handling errors
+router.route('/')
+    .post(postController.createNewPost)
+    .get(postController.getAllPosts);
 
-router.get("/", (req,res) => {
-    Post.find( (err, posts) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(posts);
-    });
-});
-
-router.get("/:id", (req,res) => {
-   const id = req.params.id;
-   Post.findById(id, (err, foundPost) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json(foundPost);
-   });
-});
-
-router.post("/", (req,res) => {
-    const blogPost = req.body;
-
-    // TODO validation
-    const post = new Post();
-    post.title = blogPost.title;
-    post.text = blogPost.text;
-
-    post.save( (err) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json({message: "Post was added", data: post});
-    } );
-});
-
-router.put("/:id", (req,res) => {
-    const id = req.params.id;
-    Post.findById(id, (err, foundPost) => {
-        if (err) {
-            res.send(err);
-        }
-
-        const title = req.body.title;
-        const text = req.body.text;
-
-        if (title) {
-            foundPost.title = title;
-        }
-
-        if (text) {
-            foundPost.text = text;
-        }
-
-        res.json(foundPost);
-    });
-});
-
-router.delete("/:id", (req,res) => {
-    const id = req.params.id;
-    Post.findByIdAndRemove(id, (err) => {
-        if (err) {
-            res.send(err);
-        }
-
-        res.json({message: "Post was deleted"});
-    })
-});
+router.route('/:id')
+    .get(postController.getPostById)
+    .put(postController.updatePost)
+    .delete(postController.deletePost);
 
 module.exports = router;
