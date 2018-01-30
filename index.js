872b95a5952fce3b320289221576fb0fb4103b4d
Vyacheslav Okulov <vokulov@intersog.com>
2018-01-30 16:10:04 +0200

    Added configuration. Update mongo to use db from mlab

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const mongoose = require('mongoose');
 const bodyParser = require("body-parser");
 const postsRouter = require("./routes/posts");
 const usersRouter = require('./routes/users');
+const keys = require('./config/keys');
+
 //app configurations
 
-mongoose.connect('mongodb://localhost:27017/myblog');
+mongoose.connect(keys.mongoURI);
 
 app.use(bodyParser.urlencoded({
     extended: true
